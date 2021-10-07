690179ed32d6d78b2aec90faa0f3d041a067d52d
Maiko Tuitupou Jr <tuitupoumaiko@gmail.com>
2021-10-07 09:08:22 -0600

    breaking change: moved rooms from being an Area2D to a rigidbody. Also refactored the Room class to fit this new specification

--- worldgen/room_controller.gd
+++ worldgen/room_controller.gd
@@ -1,40 +1,58 @@
+#
+# Represents a simple room with a randomly generated size for world generation.
+#
 class_name Room
-extends Area2D
+extends RigidBody2D
 
+
+# Singleton references
 onready var _settings := get_node("/root/WorldGenSettings")
 onready var _rng := get_node("/root/RNGManager")
 
+# Color and size variables
 export var color: Color = Color.white
-export var min_width: int = 200
-export var min_height: int = 150
+export var min_height: int = 8
+export var min_width: int = 10
 
-export var rng_mean: float = 800.0
-export var rng_deviation: float = 500
+# RNG variables (for standard deviation rng)
+export var rng_mean: float = 12
+export var rng_deviation: float = 5
 
-var shape: RectangleShape2D
+# The size of this shape (width x height)
+var size: Vector2
 
-var height = -1
-var width = -1
-
-var _rect;
+# The rectangle to draw
+var _rect: Rect2
 
 
+# Called after all children are ready at start of program
 func _ready():
+	# Calculate and store a random size that fits in the grid
+	var height = -INF
+	var width = -INF
 	while height < min_width:
-		height = _settings.float_world_to_grid(_rng.randfn(rng_mean, rng_deviation));
+		height = floor(_rng.randfn(rng_mean, rng_deviation))
 	while width < min_height:
-		width = _settings.float_world_to_grid(_rng.randfn(rng_mean, rng_deviation));
-
-	shape = RectangleShape2D.new()
-	shape.extents = _settings.vector_world_to_grid(Vector2(width/2, height/2));
+		width = floor(_rng.randfn(rng_mean, rng_deviation))
+	size = Vector2(height, width) * _settings.grid_size
 	
-	$CollisionShape2D.shape = shape;
+	# Create the collision shape based on this size
+	var shape = RectangleShape2D.new()
+	shape.extents = size
+	$CollisionShape2D.shape = shape
 	
-	_rect = Rect2(-shape.extents.x, -shape.extents.y, 2*shape.extents.x, 2*shape.extents.y)
+	# Create the drawable rectangle based on the size
+	_rect = Rect2(-size.x, -size.y, 2*size.x, 2*size.y)
 
 
+# Called almost every frame
+func _process(delta):
+	update() # Redraws the shape
+
+
+# Redraws the rectangle
 func _draw():
-	draw_rect(_rect, color);
+	draw_rect(_rect, color, false);
 
-func _get_shape():
-	return $CollisionShape2D.shape;
+
+
